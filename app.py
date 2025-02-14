b77ae11d777ccbc7c5c84921194f797b4308a229
Darryl See Wei Shen <59615070+peanutsee@users.noreply.github.com>
2025-02-14 06:57:13 +0000

    updated requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, render_template
 import google.generativeai as genai
-from dotenv import dotenv_values
-
-config = dotenv_values(".env") 
+import os 
 
 app = Flask(__name__)
 
@@ -18,15 +16,12 @@ def makersuite():
 @app.route('/gemini', methods=['GET', 'POST'])
 def gemini():
     q = request.form.get('query', "")
-    print(q)
-    API = config.get('API')
-
+    API = os.environ.get('API')
     genai.configure(api_key=API)
-
     MODEL = genai.GenerativeModel(model_name='gemini-1.5-flash')
     r = MODEL.generate_content(q)
     cleaned_r = r.candidates[0].content.parts[0].text
-    print(cleaned_r)
+
     return render_template('gemini.html', response=cleaned_r)
 
 # Run the App
